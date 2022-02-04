7e412f2c76f1ca1ba02554df0f0c375860ecab4a
ronamit <Wup31345>
2022-02-04 19:43:17 +0200

    up

--- data/avsg_transforms.py
+++ data/avsg_transforms.py
@@ -4,17 +4,19 @@ import numpy as np
 
 class set_actors_num_and_shuffle(object):
     """
-
+    take the closest max_num_agents agents to the ego (the agents are ordered this way in the data)
+    also shuffle their indexing
     """
-
     def __init__(self, opt):
         self.max_num_agents = opt.max_num_agents
 
     def __call__(self, sample):
         agents_feat = sample['agents_feat']
         agents_num_orig = agents_feat['agents_num']
-        inds = np.random.shuffle(np.arange(agents_num_orig))[:self.max_num_agents]  # take the closest agent to the ego
-        sample['agents_feat']['agents_num'] = len(inds)
+        agents_num = min(agents_num_orig, self.max_num_agents)
+        closest_agents_inds = np.arange(agents_num)
+        inds = np.random.shuffle(closest_agents_inds)
+        sample['agents_feat']['agents_num'] = agents_num
         sample['agents_feat']['agents_data'] = sample['agents_feat']['agents_data'][inds]
         return sample
 
