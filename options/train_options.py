75004d7f76570eda57ab6760574ad03d102c261f
ronamit <Was21235>
2022-02-10 16:20:44 +0200

    up

--- options/train_options.py
+++ options/train_options.py
@@ -30,7 +30,7 @@ class TrainOptions(BaseOptions):
 
         parser.add_argument('--optimizer_type', type=str, default='Adam', help='SGD / Adam')
         parser.add_argument('--lr_G', type=float, default=0.0002, help='initial learning rate for ADAM optimizer of G')
-        parser.add_argument('--lr_D', type=float, default=0.0002, help='initial learning rate for ADAM optimizer of D')
+        parser.add_argument('--lr_D', type=float, default=0.00002, help='initial learning rate for ADAM optimizer of D')
         parser.add_argument('--sgd_momentum', type=float, default=0.9, help='momentum')
         parser.add_argument('--beta1', type=float, default=0.5, help='momentum term of adam')
         parser.add_argument('--gan_mode', type=str, default='wgangp', help='the type of GAN objective. [vanilla| lsgan | wgangp]. vanilla GAN loss is the cross-entropy objective used in the original GAN paper.')
@@ -40,10 +40,10 @@ class TrainOptions(BaseOptions):
 
         parser.add_argument('--reconstruct_loss_type', type=str, default='L1', help=" 'L1' | 'MSE' ")
         parser.add_argument('--lamb_loss_G_reconstruct', type=float, default=0, help='weight for reconstruct_loss ')
-        parser.add_argument('--lamb_loss_D_grad_penalty', type=float, default=1., help='weight for gradient penalty in WGANGP')
+        parser.add_argument('--lamb_loss_D_grad_penalty', type=float, default=10., help='weight for gradient penalty in WGANGP')
         parser.add_argument('--type_weights_norm_D', type=str, default="Frobenius",
                             help=" None / Frobenius / L1 / Nuclear")
-        parser.add_argument('--type_weights_norm_G', type=str, default="None",
+        parser.add_argument('--type_weights_norm_G', type=str, default="Frobenius",
                             help=" None / Frobenius / L1 / Nuclear")
         parser.add_argument('--lamb_loss_D_weights_norm', type=float, default=1e-4, help=" ")
         parser.add_argument('--lamb_loss_G_weights_norm', type=float, default=0, help=" ")
