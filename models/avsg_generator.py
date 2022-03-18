04bada7fce370b1c7ab2ba25494aaef22a4ad43f
ronamit <Wup31345>
2022-03-18 05:37:44 +0200

    up

--- models/avsg_generator.py
+++ models/avsg_generator.py
@@ -87,8 +87,33 @@ class SceneGenerator(nn.Module):
 
         return latent_noise
 
-    ###############################################################################
 
+###############################################################################
+
+def get_distance_to_closest_lane_points(lanes_points, lanes_points_exists, reference_points):
+    '''
+    returns distances from the "reference_points" to the corresponding closest left/right lane points
+    note: reference_points changes dimensions
+    '''
+    batch_size, max_num_elem, max_points_per_elem, coord_dim = lanes_points.shape
+    max_n_agents = reference_points.shape[1]
+    invalids = torch.logical_not(lanes_points_exists)
+
+    # change the two tensors into a common shape:
+    lanes_points = lanes_points.view(batch_size, max_num_elem * max_points_per_elem, coord_dim)
+    lanes_points = lanes_points.unsqueeze(1).expand(-1, max_n_agents, -1, -1)
+    invalids = invalids.unsqueeze(1).repeat(1, max_n_agents, max_points_per_elem)
+    reference_points = reference_points.unsqueeze(2).expand(-1, -1, max_num_elem * max_points_per_elem, -1)
+
+    # find min dist from the "reference_points" to a left \ right lane point
+    d_sqr_ref_to_lane = (lanes_points - reference_points).square().sum(dim=-1)
+    d_sqr_ref_to_lane[invalids] = torch.inf  # Set distance=inf in non-existent points
+    d_sqr_ref_to_closest_lane_point = d_sqr_ref_to_lane.min(dim=-1).values
+
+    return d_sqr_ref_to_closest_lane_point
+
+
+###############################################################################
 
 def get_out_of_road_penalty(conditioning, agents, opt):
     polygon_types = opt.polygon_types
@@ -102,13 +127,10 @@ def get_out_of_road_penalty(conditioning, agents, opt):
     map_elems_exists = map_feat['map_elems_exists']
     batch_size, n_polygon_types, max_num_elem, max_points_per_elem, coord_dim = map_elems_points.shape
     agents_exists = conditioning['agents_exists']
-    n_agents_in_scene = conditioning['n_agents_in_scene']
     max_n_agents = agents.shape[1]
 
-    # Get lanes points, set infinity in non-valid coordinates
+    # Get lanes points
     lanes_mid_points = map_elems_points[:, i_lanes_mid]
-    lanes_left_points = map_elems_points[:, i_lanes_left]
-    lanes_right_points = map_elems_points[:, i_lanes_right]
 
     # Get agents centroids, set infinity in non-valid coordinates
     agents_centroids = agents[:, :, [i_centroid_x, i_centroid_y]]
@@ -141,27 +163,14 @@ def get_out_of_road_penalty(conditioning, agents, opt):
     closest_mid_points = lanes_mid_points[torch.arange(lanes_mid_points.shape[0]), i_closest_mid, :]
     closest_mid_points = closest_mid_points.view(batch_size, max_n_agents, coord_dim)
 
-    lanes_left_points = lanes_left_points.view(batch_size, max_num_elem * max_points_per_elem, coord_dim)
-    lanes_left_points = lanes_left_points.unsqueeze(1).expand(-1, max_n_agents, -1, -1)
-    lanes_right_points = lanes_right_points.view(batch_size, max_num_elem * max_points_per_elem, coord_dim)
-    lanes_right_points = lanes_right_points.unsqueeze(1).expand(-1, max_n_agents, -1, -1)
-    closest_mid_points = closest_mid_points.unsqueeze(2).expand(-1, -1, max_num_elem * max_points_per_elem, -1)
-
     # find min dist from the "closest_mid_points" to a left \ right lane point
-    d_sqr_agent_to_left = (lanes_left_points - closest_mid_points).square().sum(dim=-1)
-    d_sqr_agent_to_right = (lanes_right_points - closest_mid_points).square().sum(dim=-1)
-    invalids = torch.logical_not(map_elems_exists[:, i_lanes_left])
-    invalids = invalids.unsqueeze(1).repeat(1, max_n_agents, max_points_per_elem)
-
-    # Set distance=inf in non-existent agents and points, so that invalid indices wouldn't be chosen in the min()
-    d_sqr_agent_to_left[invalids] = torch.inf  # Set distance=inf in non-existent points
-    invalids = torch.logical_not(map_elems_exists[:, i_lanes_right])
-    invalids = invalids.unsqueeze(1).repeat(1, max_n_agents, max_points_per_elem)
-    d_sqr_agent_to_right[invalids] = torch.inf  # Set distance=inf in non-existent points
-    d_sqr_agent_to_left = d_sqr_agent_to_left.min(dim=-1).values
-    d_sqr_agent_to_right = d_sqr_agent_to_right.min(dim=-1).values
-
-    # penalize fake agents if there is any left-lane or right-lane point closer to mid_point than the agents' centroid
+    d_sqr_agent_to_left = get_distance_to_closest_lane_points(map_elems_points[:, i_lanes_left],
+                                                              map_elems_exists[:, i_lanes_left],
+                                                              closest_mid_points)
+    d_sqr_agent_to_right = get_distance_to_closest_lane_points(map_elems_points[:, i_lanes_right],
+                                                               map_elems_exists[:, i_lanes_right],
+                                                               closest_mid_points)
+    # get valid indices
     d_sqr_agent_to_left = d_sqr_agent_to_left.flatten()
     d_sqr_agent_to_right = d_sqr_agent_to_right.flatten()
     d_sqr_agent_to_closest_mid = d_sqr_agent_to_closest_mid.flatten()
@@ -169,6 +178,8 @@ def get_out_of_road_penalty(conditioning, agents, opt):
                + torch.isinf(d_sqr_agent_to_left) + torch.isnan(d_sqr_agent_to_left) \
                + torch.isinf(d_sqr_agent_to_right) + torch.isnan(d_sqr_agent_to_right)
     valids = torch.logical_not(invalids)
+
+    # penalize fake agents if there is any left-lane or right-lane point closer to mid_point than the agents' centroid
 
     # sum over all scenes and all agents the penalty for out-of-road agents
     # penalty := ELU(dist_agent_to_mid - left_to_mid) + ELU(dist_agent_to_mid - right_to_mid)
