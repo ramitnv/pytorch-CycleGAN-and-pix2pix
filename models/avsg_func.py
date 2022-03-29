eeb84eeafb2b5db38e8f4b4250e47d5e8f43bd3a
ronamit <ronamit@campus.technnion.ac.il>
2022-03-29 11:24:51 +0300

    up

--- models/avsg_func.py
+++ models/avsg_func.py
@@ -115,6 +115,7 @@ def get_out_of_road_penalty(conditioning, agents, opt):
     penalty = elu(sqrt(d_sqr_agent_to_mid[valids]) - sqrt(d_sqr_mid_to_left[valids])).sum() \
               + elu(sqrt(d_sqr_agent_to_mid[valids]) - sqrt(d_sqr_mid_to_right[valids])).sum()
 
+    penalty /= batch_size  # we want average over batch_size
     return penalty
 
 
@@ -159,29 +160,32 @@ def get_collisions_penalty(conditioning, agents, opt):
     front_direction = agents[:, :, [i_yaw_cos, i_yaw_sin]]
     front_vec = front_direction * extent_length * 0.5
     rot_mat = torch.tensor(([0, -1.], [1., 0])).to(
-        opt.device)  # explanation: the original direction vec is (cos(a), sin(a)) we want to rotate by +90 degrees, (cos(pi/2+a), sin(pi/2 + a) = (-sin(a) , +cos(a)) = rot_mat @ (cos(a), sin(a))
+        opt.device)  # explanation: the original direction vec is (cos(a), sin(a)) we want to rotate by +90 degrees,
+    # (cos(pi/2+a), sin(pi/2 + a) = (-sin(a) , +cos(a)) = rot_mat @ (cos(a), sin(a))
     left_vec = front_direction @ rot_mat * extent_width * 0.5
 
     # find the  middle of each of the 4 segments (sides of the car)
     segs_mids = {'front': centroids + front_vec, 'back': centroids - front_vec,
-               'left': centroids + left_vec, 'right': centroids - left_vec}
+                 'left': centroids + left_vec, 'right': centroids - left_vec}
 
     # find a vector that goes from the center of each segment to one of its edges (doesn't matter which of the two)
     segs_vecs = {'front': + left_vec, 'back': - left_vec,
-                'left': - front_vec, 'right': + front_vec}
+                 'left': - front_vec, 'right': + front_vec}
+
+    penalty = torch.zeros(batch_size, device=opt.device)
 
     for i_agent1 in range(max_n_agents - 1):
         for i_agent2 in range(i_agent1 + 1, max_n_agents):
-            # find valid scenes = both agents exists,
+            # find valid scenes = both agents IDs exists,
             valids = agents_exists[:, i_agent1] * agents_exists[:, i_agent2]
             for seg1_name, seg1_vecs in segs_vecs.items():
                 for seg2_name, seg2_vecs in segs_vecs.items():
                     seg1_mids = segs_mids[seg1_name]
                     seg2_mids = segs_mids[seg2_name]
-                    L1_p = seg1_mids[valids, i_agent1, :]
-                    L2_p = seg2_mids[valids, i_agent2, :]
-                    L1_v = seg1_vecs[valids, i_agent1, :]
-                    L2_v = seg2_vecs[valids, i_agent2, :]
+                    L1_p = seg1_mids[:, i_agent1, :]
+                    L2_p = seg2_mids[:, i_agent2, :]
+                    L1_v = seg1_vecs[:, i_agent1, :]
+                    L2_v = seg2_vecs[:, i_agent2, :]
 
                     # find the deviation of the intersection point from the middle of the segment
                     # See: https://math.stackexchange.com/a/406895
@@ -204,14 +208,14 @@ def get_collisions_penalty(conditioning, agents, opt):
                     # s = A^{-1} d
                     # s1 = (1/determinant) * (-L2_v_y * dx + L2_v_x * dy) = (L2_v_x * dy - L2_v_y * dx) / determinant
                     # s2 = (1/determinant) * (-L1_v_y * dx + L1_v_x * dy) = (L1_v_x * dy - L1_v_y * dx) / determinant
-                    d = L2_p[is_cross] - L1_p[is_cross]
-                    s1 = (L2_v[:, 0] * d[:, 1] - L2_v[:, 1] * d[:, 0]) / determinant
-                    s2 = (L1_v[:, 0] * d[:, 1] - L1_v[:, 1] * d[:, 0]) / determinant
+                    d = L2_p - L1_p
+                    s1 = (L2_v[:, 0] * d[:, 1] - L2_v[:, 1] * d[:, 0]) / determinant[:]
+                    s2 = (L1_v[:, 0] * d[:, 1] - L1_v[:, 1] * d[:, 0]) / determinant[:]
 
+                    # s1,s2 are the distances of the intersections from the middle of the corresponding segments
                     # if the intersection is in both segment (|s1| < 1 and |s2| < 1),
-                    # then it is a collision between the cars
-                    # add to the penalty the distance of the impact from the corner,
-                    # use ReLU or ELU to only penalize collisions
-                    pass
-
-    return
+                    # then it is a collision between the cars and a penalty is added
+                    penalty_curr = elu(1 - s1.abs()) * elu(1 - s2.abs())
+                    penalty += torch.sum(penalty_curr[valids * is_cross])  # sum over valid scenes
+    penalty /= batch_size  # we want average over batch_size
+    return penalty
